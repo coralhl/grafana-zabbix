3ae4f78aa091a5589669ab4296f1499a2e193703
coral <coral@xciii.ru>
2026-07-22 09:42:49 +0300

    HistoryPoint ItemID int64

--- pkg/zabbix/models.go
+++ pkg/zabbix/models.go
@@ -78,7 +78,7 @@ type TrendPoint struct {
 type History []HistoryPoint
 
 type HistoryPoint struct {
-	ItemID string  `json:"itemid,omitempty"`
+	ItemID int64  `json:"itemid,omitempty"`
 	Clock  int64   `json:"clock,omitempty,string"`
 	Value  float64 `json:"value,omitempty,string"`
 	NS     int64   `json:"ns,omitempty,string"`
